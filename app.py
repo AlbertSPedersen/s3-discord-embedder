6ed980e9fe43e14077663a7ce78205ba1ae0f260
AlbertSPedersen <40433209+AlbertSPedersen@users.noreply.github.com>
2021-02-08 13:30:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,13 @@ def index(object_path):
                     <meta name="theme-color" content="#00FF00">
                     <meta property="og:title" content="{object_path.split('/')[-1]}">
                     <meta content="{config['s3_endpoint'] + object_path}" property="og:image">
-                    <link type="application/json+oembed" href="https://cdn.asp.gg/oembed/{object_path}" />
+                    <style type="application/json+oembed">
+                    '{'
+                        "type": "{'photo' if response.headers.get('content-type').startswith('image') else 'video' if response.headers.get('content-type').startswith('video') else 'link'}",
+                        "author_name": "{size_string(int(response.headers.get('content-length'))) + ' | ' + response.headers.get('content-type')}",
+                        "provider_name": "{response.headers.get('last-modified')}"
+                    '}'
+                    </style>
                 </head>
             </html>'''
         else:
@@ -48,7 +54,7 @@ def oembed(object_path):
         if request.headers.get('user-agent') == 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)':
             return jsonify(
                 {
-                    "type": 'photo' if response.headers.get('content-type').startswith('image') else 'video' if response.headers.get('content-type').startswith('video') else 'link',
+                    'type': 'photo' if response.headers.get('content-type').startswith('image') else 'video' if response.headers.get('content-type').startswith('video') else 'link',
                     'author_name': size_string(int(response.headers.get('content-length'))) + ' | ' + response.headers.get('content-type'),
                     'provider_name': response.headers.get('last-modified')
                 }
