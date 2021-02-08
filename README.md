d2498c4ce402e1533cd0a5b23ce7b1f7408e1627
AlbertSPedersen <40433209+AlbertSPedersen@users.noreply.github.com>
2021-02-08 11:12:48 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# s3-discord-embedder
+# S3 Discord Embedder
+A poorly written Flask app that makes fancy Discord embeds from S3 objects, aye!
+
+### `config.json`
+```json
+{
+    "app_url": "https://cdn.example.com",
+    "s3_endpoint": "https://example.fra1.digitaloceanspaces.com/",
+    "bind_address": "0.0.0.0",
+    "bind_port": 80
+}
+```
---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, redirect, abort
+from pathlib import Path
+import requests
+import json
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+with open(str(Path(__file__).parent) + '/config.json', 'r') as file:
+    config = json.load(file)
+
+
+def size_string(size):
+    if size < 1024:
+        return str(size) + 'B'
+    elif size < 1048576:
+        return str(round(size / 1024, 2)) + 'KiB'
+    elif size < 1073741824:
+        return str(round(size / 1048576, 2)) + 'MiB'
+    else:
+        return str(round(size / 1073741824, 2)) + 'GiB'
+
+
+@app.route('/<path:object_path>')
+def index(object_path):
+    response = requests.head(config['s3_endpoint'] + object_path)
+    if response.status_code != 403:
+        if request.headers.get('user-agent') == 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)':
+            return f'''<!DOCTYPE html>
+            <html>
+                <head>
+                    <meta name="theme-color" content="#00FF00">
+                    <meta property="og:title" content="{object_path.split('/')[-1]}">
+                    <meta content="{config['s3_endpoint'] + object_path}" property="og:image">
+                    <link type="application/json+oembed" href="https://cdn.asp.gg/oembed/{object_path}" />
+                </head>
+            </html>'''
+        else:
+            return redirect(config['s3_endpoint'] + object_path)
+    else:
+        abort(404)
+
+
+@app.route('/oembed/<path:object_path>')
+def oembed(object_path):
+    response = requests.head(config['s3_endpoint'] + object_path)
+    if response.status_code != 403:
+        if request.headers.get('user-agent') == 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)':
+            return jsonify(
+                {
+                    "type": 'photo' if response.headers.get('content-type').startswith('image') else 'video' if response.headers.get('content-type').startswith('video') else 'link',
+                    'author_name': size_string(int(response.headers.get('content-length'))) + ' | ' + response.headers.get('content-type'),
+                    'provider_name': response.headers.get('last-modified')
+                }
+            )
+        else:
+            return redirect(config['s3_endpoint'] + object_path)
+    else:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host=config['bind_address'], port=config['bind_port'])
